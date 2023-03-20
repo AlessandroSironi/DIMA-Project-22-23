aac5d56be658f3315e3c4d253afd54149f49a2ca
Alessandro Sironi <100214750+AlessandroSironi@users.noreply.github.com>
2023-03-20 15:09:12 +0100

    Created Project

---
+++ MacroTracker/MacroTracker/MacroTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacroTrackerApp.swift
+//  MacroTracker
+//
+//  Created by Alessandro Sironi on 20/03/23.
+//
+
+import SwiftUI
+
+@main
+struct MacroTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
