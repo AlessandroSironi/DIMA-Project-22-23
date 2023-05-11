e5d55107a588f22baea6166274774cb940517d38
Alessandro Sironi <100214750+AlessandroSironi@users.noreply.github.com>
2023-05-11 21:51:02 +0200

    Notification Implementation

---
+++ macro_tracker/lib/services/local_notification_service.dart
@@ -0,0 +1,75 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:rxdart/subjects.dart';
+import 'package:timezone/timezone.dart' as tz;
+import 'package:timezone/data/latest.dart' as tz;
+
+
+//https://youtu.be/Yrq2llD2Ldw
+//https://pub.dev/packages/flutter_local_notifications
+
+class LocalNotificationService {
+  LocalNotificationService();
+
+  final _localNotificationService = FlutterLocalNotificationsPlugin();
+
+  Future<void> initialize() async {
+    tz.initializeTimeZones();
+    const AndroidInitializationSettings androidInitializationSettings = AndroidInitializationSettings('notifications'); //TODO: check if ok
+    DarwinInitializationSettings iosInitializationSettings = DarwinInitializationSettings(
+      requestAlertPermission: true,
+      requestBadgePermission: true,
+      requestSoundPermission: true,
+      onDidReceiveLocalNotification: onDidReceiveLocalNotification,
+    );
+
+    final InitializationSettings settings = InitializationSettings(
+      android: androidInitializationSettings,
+      iOS: iosInitializationSettings
+      );
+
+    await _localNotificationService.initialize(settings, onDidReceiveNotificationResponse: onDidReceiveNotificationResponse);
+  }
+
+  Future<NotificationDetails> _notificationDetails() async {
+    const AndroidNotificationDetails androidNotificationDetails = AndroidNotificationDetails(
+      'channel_id',
+      'channel_name',
+      channelDescription: 'description',
+      importance: Importance.defaultImportance,
+      priority: Priority.defaultPriority,
+      playSound: false);
+
+      const DarwinNotificationDetails darwinNotificationDetails = DarwinNotificationDetails();
+
+      return const NotificationDetails(android: androidNotificationDetails, iOS: darwinNotificationDetails);
+  }
+
+  void onDidReceiveNotificationResponse(NotificationResponse notificationResponse) {
+
+  }
+
+  Future<void> showNotification(int id, String title, String body) async {
+    final details = await _notificationDetails();
+    await _localNotificationService.show(id, title, body, details);
+  }
+
+  Future<void> showScheduledNotification ({
+    required int id, 
+    required String title, 
+    required String body, 
+    required int seconds
+  }) async {
+    final details = await _notificationDetails();
+    await _localNotificationService.zonedSchedule(id, title, body, tz.TZDateTime.from(DateTime.now().add(Duration(seconds:seconds)), tz.local), details, uiLocalNotificationDateInterpretation: UILocalNotificationDateInterpretation.absoluteTime);
+  }
+
+  void onDidReceiveLocalNotification (int id, String? title, String? body, String? payload) {
+    print('id $id');
+  }
+
+  void onSelectNotification (String? payload) {
+    print('payload $payload');
+  }
+}
+  
