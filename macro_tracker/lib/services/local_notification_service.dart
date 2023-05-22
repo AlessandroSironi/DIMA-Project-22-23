e23454c3fea312a7fc0c96f7c9daba5e6020fe6c
sprocatitommaso <100316970+sprocatitommaso@users.noreply.github.com>
2023-05-22 15:53:13 +0200

    Update local_notification_service.dart

--- macro_tracker/lib/services/local_notification_service.dart
+++ macro_tracker/lib/services/local_notification_service.dart
@@ -7,8 +7,6 @@ import 'package:timezone/data/latest.dart' as tz;
 //https://youtu.be/Yrq2llD2Ldw
 //https://pub.dev/packages/flutter_local_notifications
 
-
-
 class LocalNotificationService {
   LocalNotificationService();
 
@@ -18,7 +16,7 @@ class LocalNotificationService {
     tz.initializeTimeZones();
     const AndroidInitializationSettings androidInitializationSettings =
         AndroidInitializationSettings(
-            '@mipmap/ic_launcher'); //TODO: check if ok
+            '@drawable/mipmap/ic_launcher'); //TODO: check if ok
     DarwinInitializationSettings iosInitializationSettings =
         DarwinInitializationSettings(
       requestAlertPermission: true,
@@ -57,12 +55,11 @@ class LocalNotificationService {
     await _localNotificationService.show(id, title, body, details);
   }
 
-  Future<void> scheduleNotification ({
-    required int id, 
-    required String title, 
-    required String body, 
-    required int seconds
-  }) async {
+  Future<void> scheduleNotification(
+      {required int id,
+      required String title,
+      required String body,
+      required int seconds}) async {
     final details = await _notificationDetails();
     await _localNotificationService.zonedSchedule(
         id,
@@ -85,43 +82,44 @@ class LocalNotificationService {
   }
 
   Future<void> scheduleDailyNotification() async {
-  var androidSettings = AndroidInitializationSettings('app_icon');
-  var iosSettings = DarwinInitializationSettings();
-  var initializationSettings =
-      InitializationSettings(android: androidSettings, iOS: iosSettings);
+    var androidSettings = AndroidInitializationSettings('app_icon');
+    var iosSettings = DarwinInitializationSettings();
+    var initializationSettings =
+        InitializationSettings(android: androidSettings, iOS: iosSettings);
 
-  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
-  flutterLocalNotificationsPlugin.initialize(initializationSettings);
+    FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+        FlutterLocalNotificationsPlugin();
+    flutterLocalNotificationsPlugin.initialize(initializationSettings);
 
-  var androidDetails = AndroidNotificationDetails(
-      'channel_id', 'channel_name',
-      importance: Importance.high);
-  var iosDetails = DarwinNotificationDetails();
-  var generalNotificationDetails =
-      NotificationDetails(android: androidDetails, iOS: iosDetails);
+    var androidDetails = AndroidNotificationDetails(
+        'channel_id', 'channel_name',
+        importance: Importance.high);
+    var iosDetails = DarwinNotificationDetails();
+    var generalNotificationDetails =
+        NotificationDetails(android: androidDetails, iOS: iosDetails);
 
-  await flutterLocalNotificationsPlugin.zonedSchedule(
-    0,
-    'Have you logged today?',
-    'Remember to log your diet today.',
-    _nextInstanceOf7PM(),
-    generalNotificationDetails,
-    androidAllowWhileIdle: true,
-    uiLocalNotificationDateInterpretation:
-        UILocalNotificationDateInterpretation.absoluteTime,
-  );
-  print("Notification Scheduled.");
-}
+    await flutterLocalNotificationsPlugin.zonedSchedule(
+      0,
+      'Have you logged today?',
+      'Remember to log your diet today.',
+      _nextInstanceOf7PM(),
+      generalNotificationDetails,
+      androidAllowWhileIdle: true,
+      uiLocalNotificationDateInterpretation:
+          UILocalNotificationDateInterpretation.absoluteTime,
+    );
+    print("Notification Scheduled.");
+  }
 
   tz.TZDateTime _nextInstanceOf7PM() {
     tz.initializeTimeZones();
     tz.setLocalLocation(tz.getLocation('Europe/Rome'));
     final now = tz.TZDateTime.now(tz.local);
-    var scheduledDate = tz.TZDateTime(tz.local, now.year, now.month, now.day, 19);
+    var scheduledDate =
+        tz.TZDateTime(tz.local, now.year, now.month, now.day, 19);
     if (scheduledDate.isBefore(now)) {
       scheduledDate = scheduledDate.add(const Duration(days: 1));
     }
     return scheduledDate;
   }
 }
-
