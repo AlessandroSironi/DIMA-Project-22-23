ac073bc602aec016f231a73f18a0f5959e8f5951
sprocatitommaso <100316970+sprocatitommaso@users.noreply.github.com>
2023-05-30 13:23:19 +0200

    add test

--- macro_tracker/test/app/add_test.dart
+++ macro_tracker/test/app/add_test.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:macro_tracker/flutter_flow/flutter_flow_icon_button.dart';
+import 'package:macro_tracker/flutter_flow/flutter_flow_widgets.dart';
 import 'package:mockito/mockito.dart';
 
-import '../mocks/pages/goal/goal_widget_mock.dart';
+import '../mocks/pages/add/add_widget_mock.dart';
 import '../test_navigator.dart';
 
 class MockNavigatorObserver extends Mock implements NavigatorObserver {}
@@ -17,29 +17,28 @@ void main() {
     reset(appNavigator);
   });
 
-  testWidgets("Display Goals", (WidgetTester tester) async {
+  testWidgets("Display Add", (WidgetTester tester) async {
     tester.binding.window.physicalSizeTestValue = Size(1656, 2688);
     await tester.pumpWidget(MaterialApp(
       home: Material(
         child: Builder(builder: (context) {
           return MediaQuery(
             data: MediaQuery.of(context).copyWith(textScaleFactor: 0.8),
-            child: GoalWidgetMock(),
+            child: AddWidgetMock(),
           );
         }),
       ),
     ));
     expect(find.byType(Scaffold), findsOneWidget);
-    expect(find.byType(Text), findsAtLeastNWidgets(5));
-    expect(find.byType(Padding), findsAtLeastNWidgets(5));
     expect(find.byType(SafeArea), findsNWidgets(2));
-    expect(find.byType(Expanded), findsWidgets);
+    expect(find.byType(SingleChildScrollView), findsAtLeastNWidgets(1));
+    expect(find.byType(Text), findsWidgets);
     expect(find.byType(Column), findsWidgets);
     expect(find.byType(Row), findsWidgets);
+
+    expect(find.byType(Padding), findsAtLeastNWidgets(3));
     expect(find.byType(Container), findsWidgets);
-    expect(find.byType(TextFormField), findsAtLeastNWidgets(4));
-    expect(find.byType(SingleChildScrollView), findsWidgets);
-    expect(find.byType(PageView), findsWidgets);
-    expect(find.byType(FlutterFlowIconButton), findsAtLeastNWidgets(8));
+
+    expect(find.byType(ListView), findsWidgets);
   });
 }
